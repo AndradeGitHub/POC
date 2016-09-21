d318f40d32b16a350b908d812583ff67cb962491
Andre Goncalves de Andrade <andre.andrade@AUDATEX.CORP>
2016-09-21 14:30:55 -0300

    MVC3Razor

---
+++ MvcApplication3Razor/MvcApplication3Razor/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication3Razor.Models;
+
+namespace MvcApplication3Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {            
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            ViewBag.MyText = "Este texto vem do controller";            
+
+            return View(); 
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult IndexRazor()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult IndexRazor(testeRazor model, string returnUrl)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                model.UserName = "Teste UserName";
+                model.Password = "Teste Password";
+            }            
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+    }
+}
