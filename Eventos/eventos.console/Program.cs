a3fd8f2244c8dca14e042ae68cb067f56d18ef69
AndradeGitHub <andradeit@gmail.com>
2016-12-16 10:55:22 -0200

    Commit Eventos

---
+++ Eventos/eventos.console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using eventos.domain;
+
+namespace eventos.console
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.ExcedeuVelocidadeSeguranca += new VelocidadeSegurancaExcedidaEventHandler(CarroLimiteVelocidadeExcedida);
+            
+            for (int i = 0; i < 3; i++)
+            {
+                carro.Acelerar(30);
+                Console.WriteLine(" Velocidade atual : {0} Kmh ", carro.Velocidade);
+            }
+
+            Console.ReadKey();            
+        }
+
+        static void CarroLimiteVelocidadeExcedida(object source, EventArgs e)
+        {
+            Carro carro = (Carro)source;
+            Console.WriteLine("O limite de velocidade foi excedido ({0}Kmh)", carro.Velocidade);
+        }
+    } 
+}
---
+++ Eventos/eventos.domain/Carro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace eventos.domain
+{
+    public delegate void VelocidadeSegurancaExcedidaEventHandler(object source, EventArgs e);
+
+    public class Carro
+    {
+        public event VelocidadeSegurancaExcedidaEventHandler ExcedeuVelocidadeSeguranca;
+
+        private int _velocidade = 0;
+        private int _velocidadeSeguranca = 70;
+
+        public int Velocidade
+        {
+            get
+            {
+                return _velocidade;
+            }
+        }
+
+        public void Acelerar(int kmh)
+        {
+            int velocidadeAnterior = _velocidade;
+            _velocidade += kmh;
+            
+            if (velocidadeAnterior <= _velocidadeSeguranca && _velocidade > _velocidadeSeguranca)            
+                OnVelocidadeSegurancaExcedida(new EventArgs());            
+        }
+
+        public virtual void OnVelocidadeSegurancaExcedida(EventArgs e)
+        {
+            if (ExcedeuVelocidadeSeguranca != null)
+                ExcedeuVelocidadeSeguranca(this, e);
+        }
+    }
+}
